70e47e8a1307403365c7a731fe838b2165debcbb
AbuKhalil95 <yahya_abukhaleel@hotmail.com>
2020-05-11 17:16:32 +0300

    Updates

---
+++ precache-manifest.7a593913bf0c376afc573b3e5768d86b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35eea1503f26d759cf68861380c14ba4",
+    "url": "/sal-web/index.html"
+  },
+  {
+    "revision": "4eacb55be210e447a85c",
+    "url": "/sal-web/static/css/main.b805a1f7.chunk.css"
+  },
+  {
+    "revision": "74a31273167abfbbc615",
+    "url": "/sal-web/static/js/2.ec2eaa7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sal-web/static/js/2.ec2eaa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eacb55be210e447a85c",
+    "url": "/sal-web/static/js/main.28087960.chunk.js"
+  },
+  {
+    "revision": "467de5bba81946a00429",
+    "url": "/sal-web/static/js/runtime-main.859686c4.js"
+  },
+  {
+    "revision": "1e99b0442583593743adb5d91d7c0669",
+    "url": "/sal-web/static/media/BebasNeue-Bold.1e99b044.ttf"
+  },
+  {
+    "revision": "30dc35833a8ab3b835403faee927c674",
+    "url": "/sal-web/static/media/BebasNeue-Book.30dc3583.ttf"
+  },
+  {
+    "revision": "b5c9e279edbebf333edcabba7f36f065",
+    "url": "/sal-web/static/media/BebasNeue-Light.b5c9e279.ttf"
+  },
+  {
+    "revision": "9042a37caa7fb0389371de54dd886b38",
+    "url": "/sal-web/static/media/BebasNeue-Regular.9042a37c.ttf"
+  },
+  {
+    "revision": "298c4b7281c85c644a20850af7df6056",
+    "url": "/sal-web/static/media/BebasNeue-Thin.298c4b72.ttf"
+  },
+  {
+    "revision": "fed2eee8ad89953f22a820facae49f74",
+    "url": "/sal-web/static/media/hit-it.fed2eee8.png"
+  },
+  {
+    "revision": "e249c27892812819f1b9a858bbb61c11",
+    "url": "/sal-web/static/media/logoC.e249c278.png"
+  },
+  {
+    "revision": "54ff739a141cbace8d11cb11ad82ac8d",
+    "url": "/sal-web/static/media/logoW.54ff739a.png"
+  }
+]);
